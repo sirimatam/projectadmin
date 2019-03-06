36259ce0887404fa3c63ab4323f0144579dfe811
sirimatam <45136358+sirimatam@users.noreply.github.com>
2019-03-06 17:12:15 +0700

    Update upload_slip.php

--- upload_slip.php
+++ upload_slip.php
@@ -19,6 +19,31 @@ if ($_GET['id']!='')
 	echo '<h>'.$order_id.'</h>';
 }
 	
+	if($_FILES["fileToUpload"])
+	{
+		if ($_FILES["fileToUpload"]["error"] > 0)
+		{
+			echo "Error: " . $_FILES["fileToUpload"]["error"] . "<br>";
+			if($_FILES["fileToUpload"]["error"]==4) echo"No file upload";
+		}
+		else
+		{
+			$fname=explode(".",$_FILES["fileToUpload"]["name"]);
+			$new_filename=$fid_prod.".".$fname[1]; 
+			move_uploaded_file($_FILES["fileToUpload"]["tmp_name"],
+			"img/" . $new_filename);
+			date_default_timezone_set("Asia/Bangkok");
+			$date = date("Y-m-d");
+			$time = date("H:i:s");
+			pg_query($db,"INSERT INTO payment (pay_slip,pay_date,pay_time,order_id,pay_check) VALUES ('$new_filename','$date','$time','$order_id','0')")
+			
+		}
+	}
+	else 
+	{
+		echo "no file";
+	}	
+
 	
 ?>
 
